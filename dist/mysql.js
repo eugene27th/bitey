9d1628974a4fe8b5517ca9c810686ccebd33c591
Eugene <evgen.monreal@gmail.com>
2025-03-19 04:18:16 +1000

    🐈💨
    nesting and boolean transform

--- dist/mysql.js
+++ dist/mysql.js
@@ -8,10 +8,69 @@ const connection = require(`mysql2/promise`).createPool(config.mysql);
 
 
 const exe = async function(query, values, params) {
-    const [result] = await connection.execute(query, values || []);
+    const [result, fields] = await connection.execute(query, values || []);
+
+    if (!Array.isArray(result)) {
+        return result;
+    };
 
     if (result.length < 1) {
         return params?.array ? [] : null;
+    };
+
+    if (fields?.length > 0) {
+        if (params?.boolean === undefined || params?.boolean === true) {
+            const enums = {};
+
+            for (const field of fields) {
+                if (field.type === 254 && (field.flags & 256) === 256) {
+                    enums[field.name] = true;
+                };
+            };
+
+            if (Object.keys(enums).length > 0) {
+                for (let i = 0; i < result.length; i++) {
+                    for (const field in enums) {
+                        const value = result[i][field];
+
+                        if (value === `true`) {
+                            result[i][field] = true;
+                        } else if (value === `false`) {
+                            result[i][field] = false;
+                        };
+                    };
+                };
+            };
+        };
+
+        if (params?.nesting === undefined || params?.nesting === true) {
+            for (let i = 0; i < result.length; i++) {
+                const input = result[i];
+
+                const output = {};
+
+                for (const key in input) {
+                    const path = key.split(`.`);
+                    const value = input[key];
+
+                    let current = output;
+
+                    for (let i = 0; i < path.length - 1; i++) {
+                        const segment = path[i];
+
+                        if (!current[segment]) {
+                            current[segment] = {};
+                        };
+
+                        current = current[segment];
+                    };
+
+                    current[path[path.length - 1]] = value;
+                };
+
+                result[i] = output;
+            };
+        };
     };
 
     if (result.length === 1 && !params?.array) {
